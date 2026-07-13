8327cb105816bb8b4bed19a169204605e87cc233
jahidul islam <jahidilislamjishad2019@gmail.com>
2026-07-14 01:54:13 +0600

    Add tests for deleting an account

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -99,3 +99,17 @@ class TestAccountService(TestCase):
         """It should not Update an Account that doesn't exist"""
         response = self.client.put(f"{BASE_URL}/0", json={})
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_delete_account(self):
+        """It should Delete an Account"""
+        account = self._create_accounts(1)[0]
+        response = self.client.delete(f"{BASE_URL}/{account.id}")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        # confirm it's really gone
+        response = self.client.get(f"{BASE_URL}/{account.id}")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_delete_account_not_found(self):
+        """It should still return 204 even if Account doesn't exist"""
+        response = self.client.delete(f"{BASE_URL}/0")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
